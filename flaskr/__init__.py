1aeed8b33e9986aacec4334b613ada97b2166bb0
Alexander Mora Ruiz <alex16mr@gmail.com>
2023-02-22 22:19:07 -0500

    setup flask jwt token

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,4 +5,8 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['JWT_SECRET_KEY'] = 'asdfuivbasjfasd-adsijfbasd'
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+
     return app
--- flaskr/app.py
+++ flaskr/app.py
@@ -2,6 +2,7 @@ from flaskr import create_app
 from flask_restful import Api
 from .modelos import db
 from .vistas import VistaCanciones, VistaCancion, VistaAlbum, VistaCancionesAlbum, VistaAlbumsUsuario
+from flask_jwt_extended import JWTManager
 
 app = create_app('default')
 app_context = app.app_context()
@@ -16,3 +17,5 @@ api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
 api.add_resource(VistaAlbum, '/album/<int:id_album>')
 api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
 api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
+
+jwt = JWTManager(app)
