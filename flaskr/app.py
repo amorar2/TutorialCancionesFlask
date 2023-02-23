f7ac277806d32864175a682925ddb6eff8fb0c50
Alexander Mora Ruiz <alex@creci.co>
2023-02-22 19:39:55 -0500

    added canciones view

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,7 @@
 from flaskr import create_app
+from flask_restful import Api
 from .modelos import db
+from .vistas import VistaCanciones, VistaCancion
 
 app = create_app('default')
 app_context = app.app_context()
@@ -7,3 +9,7 @@ app_context.push()
 
 db.init_app(app)
 db.create_all()
+
+api = Api(app)
+api.add_resource(VistaCanciones, '/canciones')
+api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
